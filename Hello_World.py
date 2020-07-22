bae0352f856dc03eabe576d7430d1a972351a2f8
Christian <jeantianic@gmail.com>
2020-07-21 23:50:26 -0400

    initial commit

---
+++ Hello_World.py
@@ -0,0 +1,11 @@
+def hello():
+    print('Hello World!')
+
+
+def intro():
+    name = input('\nPlease what is your name?: ')
+    print(f'Hello {name}, welcome to a real-life application of Python Programming!')
+
+
+hello()
+#intro()
